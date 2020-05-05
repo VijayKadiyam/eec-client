12fe2d7334c043037561e0928b5602560ad1434c
VijayKadiyam <freelancervijayk@gmail.com>
2020-05-05 22:20:49 +0530

    Latest commit

---
+++ pages/organizations/_organizationId/screenings/create.vue
@@ -0,0 +1,167 @@
+<template>
+  <section>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">
+                <back-button
+                  :link="`/organizations/${organization.value}/incident-reports`"
+                  title="Hi"
+                >
+                </back-button>
+                Screning
+              </h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item">
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+                <li class="breadcrumb-item">
+                  <nuxt-link :to="`/organizations/${this.organization.value}/incident-reports`">Screening</nuxt-link>
+                </li>
+                <li class="breadcrumb-item active">Create</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row justify-content-center">
+            <!-- left column -->
+            <div class="col-md-6">
+              <!-- jquery validation -->
+              <div class="card card-primary">
+                <div class="card-header">
+                  <h3 class="card-title">Add Screning</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <div class="card-body">
+                  <div class="form-group">
+                    <label class="form-label">Ship Name</label>
+                    <input type="text" class="form-control" placeholder="Enter ship name"
+                      v-model="form.ship_name"
+                    >
+                    <span class="help-block" 
+                      v-if="errors.ship_name"
+                    >{{ errors.ship_name[0] }}</span>
+                  </div>
+                  <div class="form-group">
+                    <label class="form-label">Report Number</label>
+                    <input type="text" class="form-control" placeholder="Enter report number"
+                      v-model="form.report_number"
+                    >
+                    <span class="help-block" 
+                      v-if="errors.report_number"
+                    >{{ errors.report_number[0] }}</span>
+                  </div>
+                  <div class="form-group">
+                    <label class="form-label">Date</label>
+                    {{ form.date }}
+                    <client-only>
+                      <date-picker
+                        placeholder="DD-MM-YYYY"
+                        :format="customDateFormatter"
+                        value="form.date"
+                      />
+                    </client-only>
+                    <span class="help-block" 
+                      v-if="errors.date"
+                    >{{ errors.date[0] }}</span>
+                  </div>
+                  <div class="form-group">
+                    <label class="form-label">Link</label>
+                    <input type="text" class="form-control" placeholder="Enter link"
+                      v-model="form.link"
+                    >
+                    <span class="help-block" 
+                      v-if="errors.link"
+                    >{{ errors.link[0] }}</span>
+                  </div>
+                  <div class="form-group">
+                    <label class="form-label">Attachment</label>
+                    <br>
+                    <input type="file" id="file" name="file" ref="file" accept=".xlsx,.xls,image/*,.doc, .docx,.ppt, .pptx,.txt,.pdf" multiple>
+                  </div>
+                  <div class="form-footer">
+                    <button class="btn btn-primary btn-block"
+                      @click="store"
+                    >Create Screening</button>
+                  </div>
+                </div>
+              </div>
+              <!-- /.card -->
+              </div>
+            <!--/.col (left) -->
+          </div>
+          <!-- /.row -->
+        </div>
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+  </section>
+</template>
+
+<script type="text/javascript">
+import BackButton from '@/components/back-button.vue'
+import moment from 'moment'
+
+export default {
+  name: 'CreateGoodPractice',
+  data: () => ({
+    form: {
+      date: ''
+    },
+    loading: false
+  }),
+  components: {
+    BackButton
+  },
+  methods: {
+    async store() {
+      try {
+        this.loading = true
+        let admin = await this.$axios.post(`/screenings`, this.form)
+        this.form.id = admin.data.data.id
+        await this.handleFileUpload()
+        this.$router.push(`/organizations/${this.organization.value}/screenings`)
+        this.loading = false
+      }
+      catch(e) {
+        this.loading = false
+      }
+    },
+    async handleFileUpload() {
+      this.attachment = this.$refs.file.files[0]
+      let formData = new FormData();
+      formData.append('screeningid', this.form.id);
+      formData.append('attachment', this.attachment);
+      await this.$axios.post('upload_screening_attachment', formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }
+      ).then(response => {
+      })
+      .catch(function(){
+        console.log('FAILURE!!');
+      });
+    },
+    customDateFormatter(date) {
+      this.form.date = moment(date).format('DD-MM-YYYY');
+      return moment(date).format('DD-MM-YYYY');
+    },
+  }
+}
+</script>
---
+++ pages/organizations/_organizationId/screenings/index.vue
@@ -0,0 +1,142 @@
+<template>
+  <section>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Screenings</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item">
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+                <li class="breadcrumb-item active">Screenings</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Manage Screenings</h3> &nbsp;
+                  <nuxt-link class="btn btn-sm btn-info float-sm-right" :to="`/organizations/${organization.value}/screenings/create`">Add New</nuxt-link>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0">
+                  <table class="table table-head-fixed table-striped">
+                    <thead>
+                      <tr>
+                        <th>Sr. No.</th>
+                        <th>Ship Name</th>
+                        <th>Report Number</th>
+                        <th @click="sort('date')">
+                          Date
+                          <i v-if="currentSortDir == 'asc'" class="fas fa-arrow-up"></i>
+                          <i v-if="currentSortDir == 'desc'" class="fas fa-arrow-down"></i>
+                        </th>
+                        <th>Link</th>
+                        <th>Attachment</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-if="loading"
+                      > 
+                        <td colspan="5">Loading...</td>
+                      </tr>
+                      <tr v-for="(item, i) in sorteditems"
+                        :key="`item${i}`"
+                      >
+                        <td>{{ i + 1 }}</td>
+                        <td>{{ item.ship_name }}</td>
+                        <td>{{ item.report_number }}</td>
+                        <td>{{ item.date }}</td>
+                        <td><a :href="item.link" target="_blank">{{ item.link }}</a></td>
+                        <td><a :href="mediaUrl + item.imagepath" target="_blank">{{ item.imagepath }}</a></td>
+                        <td class="w-1">
+                          <nuxt-link title="Edit Details" class="icon" :to="`/organizations/${organization.value}/screenings/${item.id}`">
+                            <i class="fa fa-edit"></i>
+                          </nuxt-link>
+                          <nuxt-link title="Delete Details" class="icon" to="">
+                            <span @click="del(item.id)">
+                              <i class="fa fa-trash"></i>
+                            </span>
+                          </nuxt-link>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+          </div>
+          <!-- /.row -->
+        </div>
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+  </section>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'ManageScreenings',
+  data:() =>  ({
+    items: [],
+    loading: true,
+    currentSort:'name',
+    currentSortDir:'asc'
+  }),
+  mounted() {
+    this.getData()
+  },
+  computed:{
+    sorteditems:function() {
+      return this.items.sort((a,b) => {
+        let modifier = 1;
+        if(this.currentSortDir === 'desc') modifier = -1;
+        if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+        if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+        return 0;
+      });
+    }
+  },
+  methods: {
+    async getData() {
+      this.items = []
+      this.loading = true
+      let items = await this.$axios.get(`/screenings`)
+      this.items = items.data.data
+      this.loading = false
+    },
+    async del(id) {
+      let r = confirm('Are you sure you want to delete the data?')
+      if(r)
+        await this.$axios.delete(`/screenings/${id}`)
+      this.getData()
+    },
+    sort(s) {
+      //if s == current sort, reverse
+      if(s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+      }
+      this.currentSort = s;
+    }
+  }
+}
+</script>
