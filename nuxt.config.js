7b19831a1d61a907731efbb486da091f87ac1cfa
VijayKadiyam <freelancervijayk@gmail.com>
2020-05-12 10:09:37 +0530

    First commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,8 +102,8 @@ module.exports = {
   ],
 
   axios: {
-    baseURL: 'https://api.aethon.aaibuzz.com/api'
-    // baseURL: 'http://localhost:8080/api'
+    // baseURL: 'https://api.aethon.aaibuzz.com/api'
+    baseURL: 'http://localhost:8080/api'
     // baseURL: 'http://192.168.29.9:8080/api'
   },
   
